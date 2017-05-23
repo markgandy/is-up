848735f7b37acde264d0b53e9cb562f96c8d357b
Mark Gandy <markgandy@users.noreply.github.com>
2017-05-23 19:15:54 +1000

    Add another healthcheck

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.pingInterval = 60 * 5; // In seconds
 exports.healthCheckInterval = 60 * 60 * 24; // In seconds
-exports.urls = ["https://thecommons.me", "https://nextbeer.now.sh"];
+exports.urls = ["https://thecommons.me", "https://nextbeer.now.sh", "https://au-api-cnc.localz.io/healthcheck"];
